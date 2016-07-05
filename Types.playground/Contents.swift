cfd68eea90a7d7a241ac270f2cec7032b608b953
dochonglo <ismaelbarry777@hotmail.com>
2016-07-05 14:39:51 -0400

    Types Playground Lab is Complete

--- Types.playground/Contents.swift
+++ Types.playground/Contents.swift
@@ -4,13 +4,13 @@ var ninthPlanet: String
 ninthPlanet = "Pluto"
 
 let fifthPlanet: String = "Jupiter"
-// fifthPlanet is a constant of type ______
+// fifthPlanet is a constant of type ___String___
 
 let sixthPlanet: String = "Saturn"
-// sixthPlanet is a constant of type ______
+// sixthPlanet is a constant of type ___String___
 
 let seventhPlanet = "Uranus"
-// seventhPlanet is a constant of type ______
+// seventhPlanet is a constant of type ___String___
 
 let numberOfCountriesInAfrica = 54
 
